debe733d4877616f4a814af20d6f6a48bb185cab
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-07 10:38:58 +0000

    day1 part 2 solved

--- Day1/main.py
+++ Day1/main.py
@@ -17,4 +17,14 @@ for item in elf_calories_data_int:
             most_calories = calories
         calories = 0
 
+elves_sort = elves.sort()
+
+# Part 1 answer
+# Find the Elf carrying the most Calories.
+# How many total Calories is that Elf carrying?
 print(most_calories)
+
+# Part 2 answer
+# Find the top three Elves carrying the most Calories.
+# How many Calories are those Elves carrying in total?
+print(sum(elves[-3:]))
