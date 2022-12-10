5596c45a00f610140a045d433746fac4ce2294b6
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-10 08:36:05 +0000

    day 4 start (to do)

--- Day4/main.py
+++ Day4/main.py
@@ -1,5 +1,15 @@
+import functions
 
+big_list = functions.get_data(strip=True)
 
+print(big_list)
+
+for idx, i in enumerate(big_list):
+    a = int(i[0])
+    b = int(i[2])
+    list_1 = [x for x in range(a) if x <= b]
+    print(list_1)
+    list_2 = []
 
 # In how many assignment pairs does one range fully contain the other?
 #
