0b272be23a38f41e8260d428eeaa2cb1bdc3344d
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-13 21:39:52 +0000

    day 4 still

--- Day4/main.py
+++ Day4/main.py
@@ -2,14 +2,40 @@ import functions
 
 big_list = functions.get_data(strip=True)
 
-print(big_list)
+pairs = 0
+
+first = []
+second = []
+
+first_sections = [[]]
+second_sections = [[]]
 
 for idx, i in enumerate(big_list):
-    a = int(i[0])
-    b = int(i[2])
-    list_1 = [x for x in range(a) if x <= b]
-    print(list_1)
-    list_2 = []
+
+    a, b = i.split(",")
+    # first.append(a)
+    # second.append(b)
+
+    a_section1, a_section2 = a.split("-")
+    b_section1, b_section2 = b.split("-")
+
+    # a_list = [int(i) for i in a.split("-")]
+    # b_list = [int(i) for i in b.split("-")]
+
+    a_list = [*range(int(a_section1), int(a_section2) + 1)]
+    # first_sections.append(a_list)
+
+    b_list = [*range(int(b_section1), int(b_section2) + 1)]
+    # second_sections.append(b_list)
+
+    sections_set = list(set(a_list) & set(b_list))
+
+    # sections_set = list(set(first_sections[idx]) & set(second_sections[idx]))
+    # print(sections_set, first_sections[idx], second_sections[idx])
+    if sections_set == a_list or sections_set == b_list:
+        pairs += 1
+
+print(pairs)
 
 # In how many assignment pairs does one range fully contain the other?
 #
