caafed2b232cf55f8bc19b57bef6292535d99776
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-14 08:10:14 +0000

    day 4 part 1 solution

--- Day4/main.py
+++ Day4/main.py
@@ -2,40 +2,44 @@ import functions
 
 big_list = functions.get_data(strip=True)
 
+
 pairs = 0
-
-first = []
-second = []
-
-first_sections = [[]]
-second_sections = [[]]
+pairs1 = 0
 
 for idx, i in enumerate(big_list):
 
     a, b = i.split(",")
-    # first.append(a)
-    # second.append(b)
 
     a_section1, a_section2 = a.split("-")
     b_section1, b_section2 = b.split("-")
 
-    # a_list = [int(i) for i in a.split("-")]
-    # b_list = [int(i) for i in b.split("-")]
-
     a_list = [*range(int(a_section1), int(a_section2) + 1)]
-    # first_sections.append(a_list)
-
     b_list = [*range(int(b_section1), int(b_section2) + 1)]
-    # second_sections.append(b_list)
 
     sections_set = list(set(a_list) & set(b_list))
 
-    # sections_set = list(set(first_sections[idx]) & set(second_sections[idx]))
-    # print(sections_set, first_sections[idx], second_sections[idx])
     if sections_set == a_list or sections_set == b_list:
         pairs += 1
 
+
+    # different way
+
+    first = [int(i) for i in a.split("-")]
+    second = [int(i) for i in b.split("-")]
+
+    if first[0] <= second[0] and first[1] >= second[1]:
+        pairs1 += 1
+        pair_found_again = True
+    elif second[0] <= first[0] and second[1] >= first[1]:
+        pairs1 += 1
+
+
+
+# wrong answer
+# I kept it so to remember that set() is unordered and messed up the solution
 print(pairs)
+# answer to Part 1
+print(pairs1)
 
 # In how many assignment pairs does one range fully contain the other?
 #
