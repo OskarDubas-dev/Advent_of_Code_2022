e7ae9abb8d7fc977afbabdda7c19956da89477ba
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-16 12:18:44 +0000

    day 5 part 2 solution

--- Day5/main.py
+++ Day5/main.py
@@ -1,5 +1,9 @@
 import re
 
+# part 1 and 2 exist in the same loop so one of them need to be commented
+# I know it's messy
+
+# had to use regex, normal method split integers into digits
 with open("input.txt", 'r') as file_local:
     local_data = file_local.readlines()[10:]
     local_data_stripped = [s.strip() for s in local_data]
@@ -23,21 +27,25 @@ crates = [
     ['R', 'D', 'G', 'C', 'P', 'B', 'Q', 'W']
 ]
 
-for i in re_rearrange_nums:
-    print(i)
-
 for idx, i in enumerate(re_rearrange_nums):
     repeat = i[0]
     pickup = i[1]
     dropoff = i[2]
-
-    while repeat != 0:
+    # part 1
+    ''' while repeat != 0:
         try:
             crates[dropoff].append(crates[pickup].pop())
             repeat -= 1
         except:
             print(f"empty list at crate number:{idx}")
-            repeat -= 1
+            repeat -= 1'''
 
+    # part 2
+    temp_list = crates[pickup][-repeat:]
+    del crates[pickup][-repeat:]
+    crates[dropoff].extend(temp_list)
+
+# part 1 and 2 solution
+# comment whatever you need
 for i in crates:
     print(i)
