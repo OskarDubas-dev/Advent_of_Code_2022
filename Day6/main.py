2bba9b065025dab01100f9cd1235d1b1b6735a7c
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-16 18:14:56 +0000

    day 6 part 2 answer

--- Day6/main.py
+++ Day6/main.py
@@ -8,7 +8,17 @@ print(data)
 counter = 0
 
 for i in range(4, len(data)):
-    s = set(data[(i-4):i])
-    #print(s, i)
+    s = set(data[(i - 4):i])
+
+    # part 1 solution
     if len(s) == 4:
         print(i)
+        break
+
+for i in range(14, len(data)):
+    s = set(data[(i - 14):i])
+
+    # part 2 solution
+    if len(s) == 14:
+        print(i)
+        break
